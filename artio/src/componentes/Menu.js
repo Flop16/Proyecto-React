0739cdc04994fd25fb996a0f42f2704ab72b6841
Flop16 <florenciauehara@gmail.com>
2023-08-16 10:36:58 -0300

    mensaje

--- artio/src/componentes/Menu.js
+++ artio/src/componentes/Menu.js
@@ -7,21 +7,29 @@ export default class Menu extends React.Component {
 
     render() {
         return(
-
-            <div className="row bg-white fixed-top px-4 py-2">
-                <div className="col-12 d-flex align-items-center">
-                    <img src="/img/Logo.png" alt="logo" className="img-fluid" />
-                    <Link to="/" className="btn btn-light mx-3"> Inicio </Link>
-                    <Link to="/equipo" className="btn btn-light mx-3"> Quiénes somos </Link>
-                    <Link to="/tratamientos" className="btn btn-light mx-3"> Tratamientos </Link>
-                    <Link to="/contacto" className="btn btn-light mx-3"> Contacto </Link>
-                    <Link to="/ubicacion" className="btn btn-light mx-3"> Dónde estamos </Link>
-                </div>
+            <div>
+                <nav class="navbar navbar-expand-md fixed-top navbar-light bg-white">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="/">
+                            <img src="/img/Logo.png" alt="logo" width="300px" />
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <Link to="/" className="nav-link" aria-current="page"> Inicio </Link>
+                                <Link to="/equipo" className="nav-link"> Quiénes somos </Link>
+                                <Link to="/tratamientos" className="nav-link"> Tratamientos </Link>
+                                <Link to="/contacto" className="nav-link"> Contacto </Link>
+                                <Link to="/ubicacion" className="nav-link"> Dónde estamos </Link>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
             </div>
         )
     }
 }
-
-
 
 
