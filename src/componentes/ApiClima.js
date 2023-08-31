da3fadd38a86c1bc22e6c4581bfa77ba23bc880a
Flop16 <florenciauehara@gmail.com>
2023-08-31 15:19:18 -0300

    mensaje

--- src/componentes/ApiClima.js
+++ src/componentes/ApiClima.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-
 import WeatherForm from './WeatherForm';
 import WeatherInfo from './WeatherInfo';
 
-
 class ApiClima extends Component {
-
     state = {
         temperature: '',
         description: '',
@@ -22,12 +19,21 @@ class ApiClima extends Component {
         const cityValue = city.value;
         const countryValue = country.value;
 
-        if (cityValue && countryValue) {
-            // metric parameter is for Celcius Unit
-            const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=1b355656a1e47ba184fb5ac1a575e608&units=metric`;
+        if (!cityValue || !countryValue) {
+            this.setState({
+                error: 'Please enter both City and Country.'
+            });
+            return;
+        }
+
+        const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=1b355656a1e47ba184fb5ac1a575e608&units=metric`;
+
+        try {
             const response = await fetch(API_URL);
+            if (!response.ok) {
+                throw new Error('Weather data not available');
+            }
             const data = await response.json();
-            console.log(data)
 
             this.setState({
                 temperature: data.main.temp,
@@ -38,25 +44,27 @@ class ApiClima extends Component {
                 country: data.sys.country,
                 error: null
             });
-        } else {
+        } catch (error) {
             this.setState({
-                error: 'Please enter a City and a Country.'
+                error: 'Error de escritura de la ciudad o país, prueba con las mayúsculas iniciales e intenta de nuevo.'
             });
         }
-
     }
 
     render() {
-        return <div className="container p-4">
-            <div className="row">
-                <div className="col-md-6 mx-auto">
-                    <WeatherForm
-                        getWeather={this.getWeather}
-                    />
-                    <WeatherInfo {...this.state} />
+        return (
+            <div className="container p-4">
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        <WeatherForm
+                            getWeather={this.getWeather}
+                        />
+                        <WeatherInfo {...this.state} />
+                        
+                    </div>
                 </div>
             </div>
-        </div>
+        );
     }
 }
 
